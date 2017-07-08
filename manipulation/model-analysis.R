29ea4b1043672b26a3c486181134ee94572e6157
Andrey Koval <koval.andrey@gmail.com>
2017-07-08 11:44:29 -0400

    elemental graph for line stable

--- manipulation/model-analysis.R
+++ manipulation/model-analysis.R
@@ -1,9 +1,8 @@
 rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
 
 # ---- load-sources ------------------------------------------------------------
-source("./scripts/functions-graphs.R")
-source("./scripts/functions-tables.R")
-source("./scripts/graph-presets.R") # pre-sets and options for graphing
+source("./scripts/graphing/graph-elemental.R")
+source("./scripts/graphing/graph-presets.R") # pre-sets and options for graphing
 
 # ---- load-packages -----------------------------------------------------------
 library(magrittr) #Pipes
@@ -25,9 +24,34 @@ names(model_result)
 mr <- model_result
 mr$parameters$unstandardized
 
+model_result$savedata_info
+ds <- model_result$savedata
+
+ds %>% dplyr::glimpse()
+
+# genrate predicted value from the generated factor scores
+ds <- ds %>% 
+  dplyr::mutate(
+    predicted  = B_FAT + LINEAR*MO + QUAD*MOSQ + SED*SEDWP + MVPA*MVPAWP +
+      CAL*CALWP100 + HT*HEIGHT_W ,
+    residual  =  FAT - predicted
+  ) 
+
+
 # ---- tweak-data ----------------------------------------
+ds %>% elemental_line(
+  variable_name = "predicted",
+  time_metric = "MO",
+  color_name="black",
+  line_alpha=1,
+  line_size =.5, 
+  smoothed = TRUE,
+  rounded_digits = 0L
+)
 
 
+
+# ---- basic-graph --------------------------
 
 # ---- define-utility-functions ---------------
 
@@ -41,12 +65,13 @@ path_gh5 <- "./data-unshared/raw/model-final-2017-07-05.gh5"
 mplus.list.variables(path_gh5) # variables in the gh5 file
 mplus.view.plots(path_gh5)  # available graphs for this type of gh5 file
 # histograms
-# mplus.plot.histogram(path_gh5, "SA") # 
+mplus.plot.histogram(path_gh5, "LINEAR") #
+mplus.plot.histogram(path_gh5, "QUAD") #
 # scatterplots
-# mplus.plot.scatterplot(path_gh5, "IA", "IB") # intercepts
-# mplus.plot.scatterplot(path_gh5, "SA", "SB") # slopes
-# mplus.plot.scatterplot(path_gh5, "IA", "SA") # physical
-# mplus.plot.scatterplot(path_gh5, "IB", "SB") # cognitive
+mplus.plot.scatterplot(path_gh5, "B_FAT", "LINEAR") 
+# mplus.plot.scatterplot(path_gh5, "SA", "SB")
+# mplus.plot.scatterplot(path_gh5, "IA", "SA") 
+# mplus.plot.scatterplot(path_gh5, "IB", "SB") 
 
 # ds <- mplus.get.data(path_gh5, "SA")
 
